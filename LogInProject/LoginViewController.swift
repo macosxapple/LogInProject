ca9ac77c6c7763be83bb77719140f60454bb6f3a
Alexander Borisov <dfrost1988@gmail.com>
2022-03-11 23:26:45 +0300

    Interface work

---
+++ LogInProject/LoginViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  LogInProject
+//
+//  Created by D-Frost on 11.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    // MARK: - IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private properties
+    private let user = "User"
+    private let password = "Password"
+    
+  // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.user = user
+    }
+    
+    // MARK: IBActions
+    @IBAction func loginButtonPressed() {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+            return
+        }
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
+    }
+    
+
+    @IBAction func showAuthorizationData(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Oops!", message: "Your name is \(user) 😉")
+            : showAlert(title: "Oops!", message: "Your password is \(password) 😉")
+    }
+    
+
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+}
+
+    // MARK: - Alert Controller
+    extension LoginViewController {
+        private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+                textField?.text = ""
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true)
+        }
+    }
+
+// MARK: - Keyboard
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            loginButtonPressed()
+        }
+        return true
+    }
+}
---
+++ LogInProject/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  LogInProject
+//
+//  Created by D-Frost on 11.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var user = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(user)!"
+    }
+
+}
