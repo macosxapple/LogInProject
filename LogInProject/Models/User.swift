d5355d62aa0d8b23277c1740045e2a2eeac1ff0b
Alexander Borisov <dfrost1988@gmail.com>
2022-03-13 14:06:05 +0300

    Interface new options

---
+++ LogInProject/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  Alexander.swift
+//  LogInProject
+//
+//  Created by D-Frost on 12.03.2022.
+//
+
+import Foundation
+
+struct User {
+    let login: String
+    let password: String
+    
+}
